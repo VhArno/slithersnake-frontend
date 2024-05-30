07aa40ebdd8cb30b50bfd5de82e42fb3c4bd2515
Jesper Degroote <jesper.degroote@student.odisee.be>
2024-05-30 13:48:34 +0200

    added router check + user change

--- src/components/pages/SgCreateRoom.vue
+++ src/components/pages/SgCreateRoom.vue
@@ -45,9 +45,9 @@ const players = ref<Player[]>([])
 const player = ref<Player>({
   id: 'player' + Math.floor(Math.random() * (10000 - 0 + 1)) + 0,
   // id: 'player' + 0,
-  username: 'test user',
+  username: 'guest' + Math.floor(Math.random() * (10000 - 0 + 1)) + 0,
   email: '',
-  level: Math.floor(Math.random() * (100 - 0 + 1)) + 0,
+  level: 0,
   highscore: 0,
   games_played: 0,
   games_won: 0,
@@ -87,8 +87,9 @@ socket.on('joinedRoom', (room: Room) => {
   sessionStorage.setItem('players', JSON.stringify(room.players))
   console.log('player joined room')
   if (params.id === room.id) {
-    players.value = room.players
-    console.log(players.value)
+    currentRoom.value = room
+    console.log(room)
+    players.value = currentRoom.value.players
     socket.emit('getPlayers', params.id)
   }
 })
